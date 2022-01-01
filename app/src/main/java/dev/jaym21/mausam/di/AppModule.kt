3c9665c97507c235d673164a75e64d713b30ba4e
jaym21 <jaynmistry21@gmail.com>
2022-01-01 20:19:32 +0530

    adding dependency injection for providing retrofit

---
+++ app/src/main/java/dev/jaym21/mausam/di/AppModule.kt
@@ -0,0 +1,19 @@
+package dev.jaym21.mausam.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+        .baseUrl()
+}
