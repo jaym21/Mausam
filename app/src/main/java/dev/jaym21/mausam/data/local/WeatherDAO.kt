d29388d39a29cbd54ff161d470362c9e8816fc8a
jaym21 <jaynmistry21@gmail.com>
2022-01-03 21:57:36 +0530

    added dao functions for inserting, deleting and getting weather

--- app/src/main/java/dev/jaym21/mausam/data/local/WeatherDAO.kt
+++ app/src/main/java/dev/jaym21/mausam/data/local/WeatherDAO.kt
@@ -1,7 +1,20 @@
 package dev.jaym21.mausam.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import io.reactivex.Flowable
 
 @Dao
 interface WeatherDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWeather(weatherEntity: WeatherEntity)
+
+    @Query("DELETE FROM weather_table")
+    suspend fun deleteAllWeather()
+
+    @Query("SELECT * FROM weather_table")
+    fun getAllWeather(): Flowable<List<WeatherEntity>>
 }
