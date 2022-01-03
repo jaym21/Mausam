8ac3fada3a4c824b63784c1aa13575126cafdb8e
jaym21 <jaynmistry21@gmail.com>
2022-01-03 21:47:27 +0530

    added weather model class for local room db

---
+++ app/src/main/java/dev/jaym21/mausam/data/local/WeatherEntity.kt
@@ -0,0 +1,18 @@
+package dev.jaym21.mausam.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "weather_table")
+data class WeatherEntity (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val all: Int,
+    val lon: Double,
+    val lat: Double,
+    val name: String,
+    val temp: Double,
+    val main: String,
+    val humidity: Int,
+    val speed: Double,
+)
