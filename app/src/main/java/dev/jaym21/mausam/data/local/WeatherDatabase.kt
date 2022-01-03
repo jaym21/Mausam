39b82d0d6d5d1b662e708d217510fd99c561affe
jaym21 <jaynmistry21@gmail.com>
2022-01-03 21:59:40 +0530

    added weather database

---
+++ app/src/main/java/dev/jaym21/mausam/data/local/WeatherDatabase.kt
@@ -0,0 +1,10 @@
+package dev.jaym21.mausam.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [WeatherEntity::class], version = 1, exportSchema = false)
+abstract class WeatherDatabase: RoomDatabase() {
+
+    abstract fun getWeatherDAO(): WeatherDAO
+}
