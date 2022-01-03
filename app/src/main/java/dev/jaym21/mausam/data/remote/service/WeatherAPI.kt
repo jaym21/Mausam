2fd9867c96528d07e553436b7811a2fd1ee595a8
jaym21 <jaynmistry21@gmail.com>
2022-01-03 17:40:38 +0530

    added api call to get weather from latitude and longitude

--- app/src/main/java/dev/jaym21/mausam/data/remote/service/WeatherAPI.kt
+++ app/src/main/java/dev/jaym21/mausam/data/remote/service/WeatherAPI.kt
@@ -1,6 +1,7 @@
 package dev.jaym21.mausam.data.remote.service
 
 import dev.jaym21.mausam.data.remote.models.responses.CityResponse
+import dev.jaym21.mausam.utils.Constants
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -14,6 +15,18 @@ interface WeatherAPI {
         @Query("units")
         unit: String = "metric",
         @Query("appid")
-        apiKey: String = "e8831806d35a5163760fe42bc92a80db"
+        apiKey: String = Constants.API_KEY
+    ): Single<CityResponse>
+
+    @GET("weather?")
+    suspend fun getWeatherByLatLng (
+        @Query("lat")
+        latitude: String,
+        @Query("lon")
+        longitude: String,
+        @Query("units")
+        unit: String = "metric",
+        @Query("appid")
+        apiKey: String = Constants.API_KEY
     ): Single<CityResponse>
 }
