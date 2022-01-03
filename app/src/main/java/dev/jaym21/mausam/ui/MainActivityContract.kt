b489a02cc1566e2c146a79ff89b1911f23c8a62b
jaym21 <jaynmistry21@gmail.com>
2022-01-03 18:00:30 +0530

    updated contract interface function

--- app/src/main/java/dev/jaym21/mausam/ui/MainActivityContract.kt
+++ app/src/main/java/dev/jaym21/mausam/ui/MainActivityContract.kt
@@ -2,5 +2,12 @@ package dev.jaym21.mausam.ui
 
 interface MainActivityContract {
 
+    interface View {
+        fun invokePresenterToCallApi()
+    }
 
+    interface Presenter {
+        fun callApiToGetWeather(cityName: String)
+        fun onActivityDestroy()
+    }
 }
