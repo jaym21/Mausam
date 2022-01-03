467685ec024a1520f85a46b3a665804fb6b5ecec
jaym21 <jaynmistry21@gmail.com>
2022-01-03 22:27:24 +0530

    added main activity implementing view interface in contract interface

--- app/src/main/java/dev/jaym21/mausam/ui/MainActivity.kt
+++ app/src/main/java/dev/jaym21/mausam/ui/MainActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.jaym21.mausam.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainActivityContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun invokePresenterToCallApi() {
+
     }
 }
