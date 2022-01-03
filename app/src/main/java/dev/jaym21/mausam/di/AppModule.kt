79eba0f964be73f883e6f0ddd79114e77f1b214e
jaym21 <jaynmistry21@gmail.com>
2022-01-03 22:01:55 +0530

    added weather database provider

--- app/src/main/java/dev/jaym21/mausam/di/AppModule.kt
+++ app/src/main/java/dev/jaym21/mausam/di/AppModule.kt
@@ -1,9 +1,12 @@
 package dev.jaym21.mausam.di
 
+import android.app.Application
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dev.jaym21.mausam.data.local.WeatherDatabase
 import dev.jaym21.mausam.data.remote.service.WeatherAPI
 import dev.jaym21.mausam.utils.Constants
 import retrofit2.Retrofit
@@ -28,4 +31,10 @@ object AppModule {
     @Singleton
     fun provideWeatherApi(retrofit: Retrofit): WeatherAPI =
         retrofit.create(WeatherAPI::class.java)
+
+    @Provides
+    @Singleton
+    fun provideWeatherDatabase(application: Application): WeatherDatabase =
+        Room.databaseBuilder(application, WeatherDatabase::class.java, "weather_database")
+            .build()
 }
