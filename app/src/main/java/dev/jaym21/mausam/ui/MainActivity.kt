65fe483229f41b0cd5b0023cca50a67afa5beb40
jaym21 <jaynmistry21@gmail.com>
2022-01-03 22:30:52 +0530

    added view binding in main activity

--- app/src/main/java/dev/jaym21/mausam/ui/MainActivity.kt
+++ app/src/main/java/dev/jaym21/mausam/ui/MainActivity.kt
@@ -3,14 +3,25 @@ package dev.jaym21.mausam.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.jaym21.mausam.R
+import dev.jaym21.mausam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), MainActivityContract.View {
+
+    private var _binding: ActivityMainBinding? = null
+    val binding: ActivityMainBinding = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding?.root)
     }
 
     override fun invokePresenterToCallApi() {
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
