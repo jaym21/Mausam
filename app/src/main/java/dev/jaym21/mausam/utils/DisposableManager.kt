d8a5f6b99930e13ca92c0cf2712cb3285ea58db8
jaym21 <jaynmistry21@gmail.com>
2022-01-03 17:58:45 +0530

    added disposable manager to dispose rxjava calls safely

---
+++ app/src/main/java/dev/jaym21/mausam/utils/DisposableManager.kt
@@ -0,0 +1,30 @@
+package dev.jaym21.mausam.utils
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+class DisposableManager {
+
+    companion object {
+
+        private lateinit var compositeDisposable: CompositeDisposable
+
+        //to provide singleton composite disposable
+        private fun getCompositeDisposable(): CompositeDisposable {
+            if (compositeDisposable == null || compositeDisposable.isDisposed) {
+                compositeDisposable = CompositeDisposable()
+            }
+            return compositeDisposable
+        }
+
+        //adding a disposable to composite disposable
+        fun add(disposable: Disposable) {
+            getCompositeDisposable().add(disposable)
+        }
+
+        //disposing to composite disposable
+        fun dispose() {
+            getCompositeDisposable().dispose()
+        }
+    }
+}
