66564d96f9f98eebef60224909b0d81c8fa0afe3
jaym21 <jaynmistry21@gmail.com>
2022-01-04 19:18:17 +0530

    added suspend functions to save latitude and longitude in datastore

--- app/src/main/java/dev/jaym21/mausam/utils/DataStoreManager.kt
+++ app/src/main/java/dev/jaym21/mausam/utils/DataStoreManager.kt
@@ -1,12 +1,31 @@
 package dev.jaym21.mausam.utils
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 
 class DataStoreManager(val context: Context) {
 
     companion object {
         private val LATITUDE = stringPreferencesKey("LATITUDE")
         private val LONGITUDE = stringPreferencesKey("LONGITUDE")
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = Constants.DATASTORE)
     }
+
+    suspend fun saveLatitude(latitude: String) {
+        context.dataStore.edit {
+            it[LATITUDE] = latitude
+        }
+    }
+
+    suspend fun saveLongitude(longitude: String) {
+        context.dataStore.edit {
+            it[LONGITUDE] = longitude
+        }
+    }
+
+
 }
