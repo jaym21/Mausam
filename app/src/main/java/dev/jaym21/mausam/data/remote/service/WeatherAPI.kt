b9d65d75961d656931d13a2bd15812a4e4d65a78
jaym21 <jaynmistry21@gmail.com>
2022-01-04 18:42:59 +0530

    added api call function to get hourly forecast

--- app/src/main/java/dev/jaym21/mausam/data/remote/service/WeatherAPI.kt
+++ app/src/main/java/dev/jaym21/mausam/data/remote/service/WeatherAPI.kt
@@ -1,6 +1,7 @@
 package dev.jaym21.mausam.data.remote.service
 
 import dev.jaym21.mausam.data.remote.models.responses.CityResponse
+import dev.jaym21.mausam.data.remote.models.responses.HourlyForecastResponse
 import dev.jaym21.mausam.utils.Constants
 import io.reactivex.Single
 import retrofit2.http.GET
@@ -29,4 +30,18 @@ interface WeatherAPI {
         @Query("appid")
         apiKey: String = Constants.API_KEY
     ): Single<CityResponse>
+
+    @GET("onecall?")
+    fun getHourlyForecast (
+        @Query("lat")
+        latitude: String,
+        @Query("lon")
+        longitude: String,
+        @Query("exclude")
+        exclude: String = "current,minutely,daily,alerts",
+        @Query("units")
+        unit: String = "metric",
+        @Query("appid")
+        apiKey: String = "e8831806d35a5163760fe42bc92a80db"
+    ): Single<HourlyForecastResponse>
 }
